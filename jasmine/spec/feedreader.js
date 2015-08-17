da8313b916681b60d88bf77a9b3f0e04d7560d80
jbuechs <janina.buechner@gmail.com>
2015-08-17 16:55:50 -0400

    complete number 8

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -42,6 +42,13 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('has a name', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe('');
+            });
+         });
+
     });
 
 
